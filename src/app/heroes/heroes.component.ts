40fab7b83f0d95e26659f51f9deb9f5d19a3beb7
or999 <13598282197@163.com>
2020-10-30 11:51:22 +0800

    ✨feat: hero详情展示

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    // tslint:disable-next-line: whitespace
-    name:'Windstorm'
-};
+  selectHero: Hero;
+  herors: Hero[] = HEROES;
   constructor() { }
-
   ngOnInit(): void {
     // 生命周期钩子，组件创建完后调用，放初始化逻辑
   }
   change(): void {
-    console.log(this.hero.name);
+    // console.log(this.hero.name);
+  }
+  onSelect(hero: Hero): void {
+    this.selectHero = hero;
   }
 
 }
