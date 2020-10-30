4c36ea7b405fc4b85ebebd0d5cfccb1bdf3a0faf
or999 <13598282197@163.com>
2020-10-30 09:34:06 +0800

    🌈style(标题): 修改了标题内容，样式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testproject';
+  title = 'testproject(Tour of Heroes)';
 }
