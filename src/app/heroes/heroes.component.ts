1e6389cd6da054a260feab7b2db862ea2f3bae49
or999 <13598282197@163.com>
2020-10-30 14:29:02 +0800

    ✨feat(服务): 增加服务模块统一管理数据

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,22 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   selectHero: Hero;
-  herors: Hero[] = HEROES;
-  constructor() { }
+  heroes: Hero[];
+  constructor( private heroService: HeroService) { }
   ngOnInit(): void {
     // 生命周期钩子，组件创建完后调用，放初始化逻辑
+    this.getHeroes();
   }
   change(): void {
     // console.log(this.hero.name);
   }
   onSelect(hero: Hero): void {
     this.selectHero = hero;
+  }
+  getHeroes(): void{
+    // this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
